d07778e558c58290389a5c4046d566b56d4340c6
shivamdevraj02 <devrajshivam02@gmail.com>
2025-05-19 21:57:30 +0530

    about_text change

--- src/pages/web/about-us/index.jsx
+++ src/pages/web/about-us/index.jsx
@@ -3,6 +3,8 @@ export default function AboutUs() {
     <>
       <section className="about-us min-h-[100vh] flex items-center justify-center">
         <h1 className="text-3xl text-center text-blue-500">About us Page</h1>
+        <h2>shivam devraj </h2>
+  
       </section>
     </>
   );
